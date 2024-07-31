ec0675a10f47d0fd36b8642451548d0894fd9e29
Jono <127705274+KangJ0n0@users.noreply.github.com>
2024-07-31 11:38:30 +0700

    profil

--- resources/views/components/profil.blade.php
+++ resources/views/components/profil.blade.php
@@ -10,7 +10,7 @@
 
 <body>
   <div class="flex flex-row bg-white">
-    <div class="flex items-center bg-[#F4F4F4] p-8 w-1/3">
+    <div class="flex items-center bg-[#F4F4F4] p-8 w-1/3" style="background-image: url('{{ asset('storage/profilki.png') }}');">
       <div>
         <h1 class="text-[#042148] font-bold text-5xl">PROFIL</h1>
         <h1 class="text-[#4095A4] font-bold text-5xl">KABUPATEN</h1>
@@ -18,19 +18,20 @@
       </div>
     </div>
     <div class="flex justify-center items-center bg-cover bg-no-repeat w-2/3 relative"
-      style="background-image: url('https://placehold.co/1920x1080');">
+      style="background-image: url('storage/profilka.png');">
       <div class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex space-x-12">
         <div class="flex flex-col items-center">
-          <img src="https://placehold.co/100x100" alt="Logo Pemerintahan" class="w-24 h-24">
+          <img src="storage/profilpemerintahan.png" alt="Logo Pemerintahan" class="w-24 h-24">
           <p class="text-white mt-2">Profil Pemerintahan</p>
         </div>
         <div class="flex flex-col items-center">
-          <img src="https://placehold.co/100x100" alt="Map Profil Kota" class="w-24 h-24">
+          <img src="storage/profilkota.png" alt="Map Profil Kota" class="w-24 h-24">
           <p class="text-white mt-2">Profil Kota</p>
         </div>
       </div>
     </div>
   </div>
+  <br>
 </body>
 
 </html>
